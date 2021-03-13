e14b5fdb25f0629b8cada06e298f459720476d0b
Ace Eusebio <aceeusebio@gmail.com>
2021-03-13 19:36:24 +0800

    Added loggers to POST methods

--- api/src/main/kotlin/com/churchqr/scheduler/EventScheduler.kt
+++ api/src/main/kotlin/com/churchqr/scheduler/EventScheduler.kt
@@ -3,6 +3,7 @@ package com.churchqr.scheduler
 import com.churchqr.model.Event
 import com.churchqr.model.enum.EventStatus
 import com.churchqr.repository.EventRepository
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime
@@ -14,17 +15,19 @@ import java.util.*
 @Component
 class EventScheduler(private val eventRepository: EventRepository) {
 
+    private val logger = LoggerFactory.getLogger(javaClass);
+
     /**
      * Opens a new event for registration
      */
     @Scheduled(cron = "0 12 * * 0")
     fun createEvent() {
         val nextSunday = LocalDateTime.now().plusDays(7).minusHours(1)
-
         val instant: Instant = nextSunday.atZone(ZoneId.systemDefault()).toInstant()
         val epochMilli = instant.toEpochMilli()
 
         val newEntity = Event(UUID.randomUUID(),"Sunday Service " + nextSunday.year + "-" + nextSunday.month + "-" + nextSunday.dayOfMonth, epochMilli, EventStatus.OPEN)
+        logger.info("EVENT OPENED: {}]", newEntity)
         eventRepository.save(newEntity)
     }
 
@@ -35,6 +38,7 @@ class EventScheduler(private val eventRepository: EventRepository) {
     fun closeEventRegistration() {
         val currentEvent = eventRepository.findTop1ByStatusOrderByEventDateTimeDesc(EventStatus.OPEN)
         val updatedEvent: Event = currentEvent.copy(status = EventStatus.CLOSED)
+        logger.info("EVENT CLOSED: {}]", updatedEvent)
         eventRepository.save(updatedEvent)
     }
 
