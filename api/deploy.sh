a3a71d61ed46ff828fb9698e1b8e8a7e85404c48
Ace Eusebio <aceeusebio@gmail.com>
2021-03-15 20:55:58 +0800

    Image name change to acelouie/*

--- api/deploy.sh
+++ api/deploy.sh
@@ -1,3 +1,3 @@
 docker stop churchqr-api || true && docker rm churchqr-api || true
-docker build -t churchqr-api:latest .
-docker run --name churchqr-api -d -p 9001:9001 churchqr-api:latest
+docker build -t acelouie/churchqr-api:latest .
+docker run --name churchqr-api -d -p 9001:9001 acelouie/churchqr-api:latest
