55807f5347949808c4c388b5b4f35a1709c79942
Ace Eusebio <aceeusebio@gmail.com>
2021-03-16 20:30:55 +0800

    Revert to old behavior

--- scanner/src/qrCodeScanner.js
+++ scanner/src/qrCodeScanner.js
@@ -14,14 +14,14 @@ let scanning = false;
 qrWindow.callback = scannedData => {
     if (scannedData) {
 
-        const userAction = async () => {
+        /*const userAction = async () => {
             const response = await fetch('https://api.ffaurora.org/api/v1/reservation?id=' + scannedData);
             const reservationDetails = await response.json();
 
             outputData.innerText = JSON.stringify(reservationDetails);
-        }
+        }*/
 
-        /*scanning = false;
+        scanning = false;
 
         video.srcObject.getTracks().forEach(track => {
             track.stop();
@@ -29,7 +29,7 @@ qrWindow.callback = scannedData => {
 
         qrResult.hidden = false;
         canvasElement.hidden = true;
-        btnScanQR.hidden = false;*/
+        btnScanQR.hidden = false;
     }
 };
 
