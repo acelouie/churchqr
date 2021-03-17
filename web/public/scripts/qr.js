4414ee23bc343aa53475320772873083b2599b61
Ace Eusebio <aceeusebio@gmail.com>
2021-03-17 21:32:47 +0800

    Better QR page

--- web/public/scripts/qr.js
+++ web/public/scripts/qr.js
@@ -2,7 +2,7 @@ var queryString = decodeURIComponent(window.location.search);
 queryString = queryString.substring(1);
 var queries = queryString.split("?");
 
-async function setStatus(){
+async function setInfo(){
 
     var getOptions = {
         method: 'GET',
@@ -11,14 +11,14 @@ async function setStatus(){
         cache: 'default'
     };
 
-    var response = await fetch(event_current, getOptions);
+    var response = await fetch(reserve + "?id=" + queries[0], getOptions);
     var result = await response.json();
 
     var qrcode = new QRCode (document.getElementById("qrcode"), queries[0]);
 
-    var p_text = document.getElementById('registrationText');
-    p_text.innerText = result.name;
+    document.getElementById('eventName').innerText = result.event.name;
+    document.getElementById('personName').innerText = result.person.firstName + " " + result.person.lastName;
 }
 
-setStatus();
+setInfo();
 
