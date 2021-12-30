5a7db9217a5070fa8a7f798e53dd91c5cd812743
Ace Eusebio <aceeusebio@gmail.com>
2021-12-30 22:49:42 +0800

    Remove schedulers to transition to Lambda

--- api/src/main/kotlin/com/eusebioapps/api/repository/EventRepository.kt
+++ api/src/main/kotlin/com/eusebioapps/api/repository/EventRepository.kt
@@ -4,7 +4,4 @@ import com.eusebioapps.api.model.Event
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface EventRepository : JpaRepository<Event, String> {
-
-    fun findTop1ByOrderByEventDateTimeDesc(): Event?
-
 }
